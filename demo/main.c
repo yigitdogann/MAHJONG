22686dc30993dd7daa4cec963193a10aa4b6c222
enginpaksoy <2023555455@ogr.cu.edu.tr>
2024-02-26 23:58:42 +0300

    blocks array has been added from txt file with errors

--- demo/main.c
+++ demo/main.c
@@ -1,21 +1,34 @@
 ﻿#include <raylib.h>
+#include <stdio.h>
+#define ROW 10
+#define COLUMN 25
+
+typedef struct block {
+	Vector2 point;
+	Vector2 size;
+	Color color;
+	int active;
+}block;
 
 const int screenWidth = 1400; //1230
 const int screenHeight = 850; //700
 
 void drawGame();
 void updateGame();
+void InitGame();
 
-typedef struct block {
-	Rectangle rect;
 
-}block;
+FILE *file[2];
+block blocks[2][10][25];
+
+
 
 int main() {
+	InitGame();
+
 	InitWindow(screenWidth, screenHeight, "MAHJONG");
 	SetTargetFPS(60);
 
-	block mohjong = { 0 };
 
 	while (!WindowShouldClose()) {
 		//updateGame();
@@ -26,15 +39,44 @@ int main() {
 }
 
 void drawGame() {
+
 	BeginDrawing();
 
 	ClearBackground(RAYWHITE);
 	DrawRectangle(85, 75, 1230, 700, BLACK);
 	DrawText("move ball with mouse and click mouse button to change color", 10, 10, 20, DARKGRAY);
 
+	for (int i = 0; i < ROW; i++){
+		for (int j = 0; j < COLUMN; j++){
+			if(blocks[0][i][j].active == 0){
+				DrawRectangleV(blocks[0][i][j].point, blocks[0][i][j].size, RED);
+			}
+		}
+	}
+
 	EndDrawing();
 }
 
 void UpdateGame() {
 
+}
+
+void InitGame() {
+	for (int i = 0; i < 2; i++) {
+		file[0] = fopen("C:/Users/Engin Paksoy/source/repos/MAHJONG/assets/map1.txt", "r");//error
+		file[1] = fopen("C:/Users/Engin Paksoy/source/repos/MAHJONG/assets/map2.txt", "r");//error
+		if (file[i] == NULL) {
+
+		}
+		for (int j = 0; j < ROW; j++){
+			for (int k = 0; k < COLUMN; k++)
+			{
+				fscanf(file[i], "%d", &blocks[i][j][k].active);//error
+				blocks[i][j][k].point.x = k * 50;
+				blocks[i][j][k].point.y = j * 70;
+				blocks[i][j][k].size.x = 50;
+				blocks[i][j][k].size.y = 70;
+			}
+		}
+	}
 }
